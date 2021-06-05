2f48217f1d2d99e0c32fb8355e186181a9f45ab4
vd-37 <62375990+vd-37@users.noreply.github.com>
2021-06-05 15:03:56 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,10 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 from requests.api import request
-response = requests.get("https://webgeeks-2.herokuapp.com/trial.html")
+response = requests.get("https://webgeeks-2.herokuapp.com/sonnet1.html")
 soup = BeautifulSoup( response.content , 'html.parser')
 classes = [value 
            for element in soup.find_all(class_=True) 
            for value in element["class"]]
 print(list(set(classes)))      
-
